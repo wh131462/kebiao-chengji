35af8b8f3665d2b1aac861b65c9c6bbf905a5928
wh131462 <hao131462@qq.com>
2019-11-04 13:40:39 +0800

    实现对易班api的调用  可记录账号密码登录

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 $token=$_GET["token"]==""?"":$_GET["token"];
-$username=$_GET["username"]==""?"":$_GET["username"];
-$password=$_GET["password"]==""?"":$_GET["password"];
+$username=$_GET["username"]==null?"":$_GET["username"];
+$password=$_GET["password"]==null?"":$_GET["password"];
 
 if($username!=""){
-	require("php/add.php");
+	require_once("php/add.php");
 	// 获取token成功
 	require_once("php/YBAPI-classes/yb-globals.inc.php");
 	// 配置文件
-	require_once 'php/config.php';
+	require_once('php/config.php');
 	//初始化配置信息，并获取token
 	$api = YBOpenApi::getInstance()->init($config['appid'], $config['seckey'], $config['backurl']);
 	$api->bind($token);
@@ -19,9 +19,9 @@ if($username!=""){
 }else{
 	if (isset($token)&&$token){
 			// 获取token成功
-			require("php/YBAPI-classes/yb-globals.inc.php");
+			require_once("php/YBAPI-classes/yb-globals.inc.php");
 			// 配置文件
-			require_once 'php/config.php';
+			require_once('php/config.php');
 			//初始化配置信息，并获取token
 			$api = YBOpenApi::getInstance()->init($config['appid'], $config['seckey'], $config['backurl']);
 			$api->bind($token);
@@ -29,24 +29,22 @@ if($username!=""){
 			$info=$api->request('user/me');
 			// 获取到真实信息之后  进行和数据库的比对
 			require_once("php/mysql.php");
-			$sql="SELECT ybid,stuid,password FROM YBS_info WHERE ybid=\"".$info["yb_userid"]."\";";//查询账号密码
-			$result=mysqli_query($conn,$sql);
-			$res=mysqli_fetch_array($result,MYSQLI_ASSOC);
+			$sql="SELECT ybid,stuid,password FROM YBS_info WHERE ybid=".$info["info"]["yb_userid"].";";//查询账号密码
+			$sqlRes=mysqli_query($conn,$sql);
+			$res=mysqli_fetch_array($sqlRes,MYSQLI_ASSOC);
 			if($res["ybid"]==null){
 				// 第一次登陆 要求输入学号密码
-				
 				include_once("php/html.php");
 				echo "<script type=\"text/javascript\" src=\"js/SubToSelf.js\"></script>";
 				include_once("php/htmlend.php");
 				
 			}else{
 				include_once("php/html.php");
-				
-				echo "<script>vue.lghidden=true;
-				vue.username=".$res["stuid"].";
-				vue.password=".$res["password"].";
-				vue.login();</script>";
 				echo "<script type=\"text/javascript\" src=\"js/work.js\"></script>";
+				echo "<script>vue.lghidden=true;
+				vue.username=\"".$res["stuid"]."\";
+				vue.password=\"".$res["password"]."\";
+				vue.login();</script>";
 				include_once("php/htmlend.php");
 			}
 			closeSql();
--- php/add.php
+++ php/add.php
@@ -13,14 +13,14 @@ function addMem($ybid,$stuid,$password,$name,$school_name,$college_name,$class_n
 }
 function add($ybid,$stuid,$password){
 	// 存入数据库  存储数据的简易版本 获取校级权限之后 可以删除此数据
-	$add="INSERT INTO YBS_info (ybid,stuid,password) VALUES('$ybid','$stuid',''$password');";
+	$add="INSERT INTO YBS_info (ybid,stuid,password) VALUES(".$ybid.",".$stuid.",".$password.");";
 	global $conn;
 	if($conn->query($add)==TRUE){
-		echo "Ins success";
+		echo "Ins success";;
 		closeSql();
 		return "Ins Success.";
 	}else{
-		echo "Ins failed";
+		echo "Ins failed". $conn->error;
 		closeSql();
 		return "Ins Failed.";
 	}
